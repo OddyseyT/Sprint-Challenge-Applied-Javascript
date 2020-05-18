73d2c3bcce5421b478597e70b64af05267712c55
Lambda_School_Loaner_282 <lambda_school_loaner_282@Lambda-School-Loaner-282s-MacBook-Pro.local>
2020-05-17 22:35:53 -0500

    First push.  Ive been forgetting to push my work up onto GitHub

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,72 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+const cardsContainer = document.querySelector('.cards-container')
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+
+.then(response => {
+    console.log(response.data)
+
+    const articleTopics = response.data.articles
+
+    const articleKeys = Object.keys(articleTopics) 
+    
+    console.log(articleKeys)
+
+
+    articleKeys.forEach( (element) => {
+        const articleList = articleTopics[element]
+
+        console.log(articleList)
+
+        articleList.forEach((i) =>
+        {
+            cardsContainer.appendChild((cardMaker(i)))
+        })
+        
+        
+    })
+
+    
+        
+        
+    
+
+
+
+    
+
+})
+.catch(err => {console.log("something went wrong")})
+
+.then(() => {console.log("success")})
+
+function cardMaker(item) {
+    const cardDiv = document.createElement('div')
+    const headDiv = document.createElement('div')
+    const authorDiv = document.createElement('div')
+    const imgBox =
+    document.createElement('div')
+    const authorImg = document.createElement('img')
+    const authName = document.createElement('span')
+
+headDiv.textContent = item.headline
+authorImg.src = item.authorPhoto
+authName.textContent = `By: ${item.authorName}`
+
+cardDiv.appendChild(headDiv)
+headDiv.appendChild(authorDiv)
+headDiv.appendChild(imgBox)
+headDiv.appendChild(authName)
+imgBox.appendChild(authorImg)
+
+cardDiv.classList.add('card')
+headDiv.classList.add('card','headline')
+authorDiv.classList.add ('card', 'author')
+imgBox.classList.add('card', 'author','img-container')
+authName.classList.add('card', 'author', 'span')
+
+
+return cardDiv
+}
